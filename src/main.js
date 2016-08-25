47ee4f0e0d2dd54df5c07420c11e190d8fa573da
Jichao <jicahoo@163.com>
2016-08-25 07:29:21 -0400

    The code worked basically.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
-var SSH = require('simple-ssh');
+//npm install ssh2
+var Client = require('ssh2').Client;
 
-var ssh = new SSH({
-    host: 'localhost',
-    user: 'username',
-    pass: 'password'
+var conn = new Client();
+conn.on('ready', function() {
+    console.log('Client :: ready');
+    conn.shell(function(err, stream) {
+        if (err) throw err;
+        stream.on('close', function() {
+            console.log('Stream :: close');
+            conn.end();
+        }).on('data', function(data) {
+            console.log('STDOUT: ' + data);
+        }).stderr.on('data', function(data) {
+            console.log('STDERR: ' + data);
+        });
+        stream.end('ls -l\nexit\n');
+    });
+}).connect({
+    host: '10.108.53.81',
+    port: 22,
+    username: 'root',
+    //password:'c4proto!',
+//tryKeyboard: true,
+    privateKey: require('fs').readFileSync('/home/c4dev/nodejs/workspace/temp_id_rsa')
 });
-
-ssh.exec('echo $PATH', {
-    out: function(stdout) {
-        console.log(stdout);
-    }
-}).start();
-
-/*** Using the `args` options instead ***/
-ssh.exec('echo', {
-    args: ['$PATH'],
-    out: function(stdout) {
-        console.log(stdout);
-    }
-}).start();
